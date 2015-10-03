3ca58f2cfc9ac2c236e9a5723a8c40c9a5d2d489
vmuser <sashs@gmx.com>
2015-10-03 14:53:50 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+When Silicon Valley Photovoltaics Society (SVPVS) merged with IEEE in late 2011, 
+I designed and developed this web site and maintained it till Jan 2015. The 
+slides from the technical talk are not included in this repository. The link 
+for the site is: http://www.ewh.ieee.org/r6/scv/pv/index.html
---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,63 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+    'bIsWebPath':true,
+    'sUrl':'index.html'
+},
+{   'sTitle':'About Us',
+    'bIsWebPath':true,
+    'sUrl':'page2.html'
+},
+{   'sTitle':'Events',
+    'bIsWebPath':true,
+    'sUrl':'page3.html',"childArray" : [
+    {   'sTitle':'2015',
+        'bIsWebPath':true,
+        'sUrl':'page19.html'
+    },
+    {   'sTitle':'2014',
+        'bIsWebPath':true,
+        'sUrl':'page18.html'
+    },
+    {   'sTitle':'2013',
+        'bIsWebPath':true,
+        'sUrl':'page17.html'
+    },
+    {   'sTitle':'2012',
+        'bIsWebPath':true,
+        'sUrl':'page16.html'
+    },
+    {   'sTitle':'2011',
+        'bIsWebPath':true,
+        'sUrl':'page12.html'
+    },
+    {   'sTitle':'2010',
+        'bIsWebPath':true,
+        'sUrl':'page13.html'
+    },
+    {   'sTitle':'2009',
+        'bIsWebPath':true,
+        'sUrl':'page14.html'
+    },
+    {   'sTitle':'2008',
+        'bIsWebPath':true,
+        'sUrl':'page15.html'
+    }]
+},
+{   'sTitle':'PV Links',
+    'bIsWebPath':true,
+    'sUrl':'page4.html',"childArray" : [
+    {   'sTitle':'Magazine Links',
+        'bIsWebPath':true,
+        'sUrl':'page10.html'
+    },
+    {   'sTitle':'Exhibitions and Conferences',
+        'bIsWebPath':true,
+        'sUrl':'page11.html'
+    }]
+},
+{   'sTitle':'Contact',
+    'bIsWebPath':true,
+    'sUrl':'page5.html'
+}]
+});
